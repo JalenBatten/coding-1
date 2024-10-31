eecb50854bb13eee4617ba98caddbcad7f158597
JalenBatten <jbatten396@boyslatin.net>
2024-10-31 13:33:05 +0000

    Introduction to Python

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,28 @@
+# data types = basic building bllock of python programming.
+# string = data represented as characters wrapped
+# together in quotation marks.
+# integer = whole numbers.
+678
+# floats = decimal numbers 
+678.99
+# boolean = true or false values
+True
+False
+# variables = containers for data
+# we create variables using the following steps:
+# 1. create a variable name
+# 2. apply an equal sign
+# 3. give it a data type value
+userName = "Jalen. "
+message ='Good Morning, '
+weather ='It is 60 degress and sunny.'
+
+# we use print to see our data in our terminal
+print(message + userName + weather)
+number1= 1200
+number2= 800
+print(number1, number2)
+name= 'My name is Jalen.'
+activity= 'I am going to an arcade.'
+time='I will do this at 2:00pm.'
+print(name, activity, time)
---
+++ classNotes/pythonDataTypes.py
@@ -0,0 +1,30 @@
+# Strings are pieces of data wrapped in quotation marks. 
+"My name is Jalen"
+"34"
+"!@@$%"
+# Integers are pieces of data that represent whole numbers.
+# whole numbers do not have decimals
+22
+45
+245
+
+# Floats
+"Floats represent pieces of data that contain a decimal."
+33.56
+50.24
+506.78
+
+# booleans
+# booleans are pieces of data that represent true or false situations
+True
+False
+
+#variables
+#variables are containers for data that can be re-used
+username=''
+password=''
+profileImg=''
+posts=[]
+{}
+[]
+()
