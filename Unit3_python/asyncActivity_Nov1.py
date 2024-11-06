70c10fe0a925977268bd9f8d6842be67e2dc3f43
JalenBatten <jbatten396@boyslatin.net>
2024-11-06 13:54:32 +0000

    Async Activity November 1st

---
+++ Unit3_python/asyncActivity_Nov1.py
@@ -0,0 +1,9 @@
+number1=125
+number2=120
+print(number1 + number2)
+Step2= "To create a calendar you would use strings for the days of the week and integers for the dates."
+Step3= "A floats are decimal numbers, while integers are whole numbers."
+Step4= "The correct data type for a comment section would be string.  The correct data type for a survey would be boolean."
+print(Step2, Step3, Step4)
+
+
