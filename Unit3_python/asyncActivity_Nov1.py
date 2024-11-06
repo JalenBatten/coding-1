0b30bfd52268448eb03fe0b990ceea1b3cffb166
JalenBatten <jbatten396@boyslatin.net>
2024-11-06 14:11:37 +0000

    Async Activity November 1st Final  Commit

--- Unit3_python/asyncActivity_Nov1.py
+++ Unit3_python/asyncActivity_Nov1.py
@@ -5,5 +5,5 @@ Step2= "To create a calendar you would use strings for the days of the week and 
 Step3= "A floats are decimal numbers, while integers are whole numbers."
 Step4= "The correct data type for a comment section would be string.  The correct data type for a survey would be boolean."
 print(Step2, Step3, Step4)
-
+# Better wifi and computers would help us get on task faster.
 
