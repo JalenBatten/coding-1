cc84780e9f987a338b9f827f2bc651a7f84fbc50
JalenBatten <jbatten396@boyslatin.net>
2024-11-13 14:35:50 +0000

    Python Operator Notes 11/13/24

---
+++ Unit3_python/operatorReview.py
@@ -0,0 +1,48 @@
+# Arithmetic operator - writing code that does math proccesses
+# an example of a computer program that uses arithmatic is a bank app
+depositAmount= 50
+withdrawAmount= 25
+
+checkingAccount= 1000
+savingsAccount= 2000
+creditAmount= 1500
+
+paypalFee= 0.25
+print(depositAmount + checkingAccount)
+
+# Assignment operators- giving variables values. KEYWORD is assignment
+# any time you are using a variable, you are using the assignment operator
+tShirt= 50.00
+discount_25=.25
+appliedDiscount= tShirt * discount_25
+totalAtCheckout= tShirt - appliedDiscount
+print(totalAtCheckout)
+
+
+# Comparison operator- is analyzing two values to determine a outcome.
+# == this means they are the same or equal 
+# != this means they are not equal
+
+
+wendysReviewScore = 4.0
+burgerKingReviewScore = 4.2
+
+# compare best reviewed resturant
+print(burgerKingReviewScore > wendysReviewScore)
+
+wendysBurgerCalories= 200
+burgerKingBurgerCarlories= 300
+
+
+# can i eat this meal today
+dailyCaloricIntake= 3000
+appleBeesMeal= 2300
+print(dailyCaloricIntake == appleBeesMeal)
+
+#comparing caloric Intake
+print (wendysBurgerCalories != burgerKingBurgerCarlories)
+
+# 1 equal sign is used for assigning values. For Assignment
+# 2 equal signs are used to compared if things are equal. For Comparison
+
+# Logical 
