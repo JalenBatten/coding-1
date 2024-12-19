5a64c802a22864b62cfda0aac062ba2e690c077e
JalenBatten <jbatten396@boyslatin.net>
2024-12-19 14:23:03 +0000

    Holiday Schedule Extra Credit

---
+++ Unit3_python/holidayExtraCredit.py
@@ -0,0 +1,19 @@
+def holidaySchedule(date):
+    if date == '12/21/24':
+        print('Play video games.')
+    elif date == '12/24/24':
+        print('Have a christmas party.')
+    elif date == '12/25/24':
+        print('Celebrate Christmas')
+    elif date == '12/26/24':
+        print('Visit family members.')
+    elif date == '12/31/24':
+        print('Celebrate New Years')
+    elif date == '1/6/24':
+        print('Get prepared for school.')
+    else:
+        print('Nothing is planned for this day.')
+
+holidaySchedule('12/21/24')
+holidaySchedule('12/25/24')
+holidaySchedule('12/26/24')
