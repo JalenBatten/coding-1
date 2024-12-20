8b5cf987be74cd6efec0965b0d1cdd5b736eb510
JalenBatten <jbatten396@boyslatin.net>
2024-12-20 20:41:53 +0000

    Quarter 2 Quiz

---
+++ Unit3_python/Quarter2_Quiz.py
@@ -0,0 +1,27 @@
+#1
+print('A function is a series of instructions or steps that tell a computer to do a specific action.')
+#2
+print('A function paramater acts as placeholder for real data that will put into the function') 
+print('On the otherhand, the function argument is the real data passed into a function call.')
+#3
+print('An if/else conditional statement are key words used to filter and form different results based on data.')
+#4
+print('The integer data type represents whole numbers, and cannot contain decimals values.')
+#5
+print('The boolean data type is used to represent true or false situations.')
+#6
+print('An age verification function that needs to check if a user is a certain age or older to purchase a video game would use the comparison operator.')
+#7
+print('An office security function that needs to have a specific alpha-numeric code to be entered in order to enter and exit a building would use the comparison operator.')
+#8
+def passwordCharacterCounter(password):
+    print(len(password))
+
+passwordCharacterCounter('w34')
+
+def incomeTaxes(income):
+    if income <=12000:
+        
+        
+    
+
