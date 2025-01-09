81ea01ff53df3c20442a9beb1358a6f8e5379f7f
JalenBatten <jbatten396@boyslatin.net>
2025-01-09 14:36:00 +0000

    Python Practice 1-9-24

--- unit 4/pythonLoops.py
+++ unit 4/pythonLoops.py
@@ -4,14 +4,17 @@
 
 def gradeToTitle():
     year = int(input ('What grade are you in.'))
+    if year == 0:
+        print(collegeTitles[0])
+    collegeTitles= ['You dont have a college title yet', 'freshman', 'sophmore', 'junior', 'senior']
     if year == 1:
-        print('You are a freshman in undergrad.')
+        print(collegeTitles[1])
     elif year == 2:
-        print('You are a sophmore in undergrad.')
+        print(collegeTitles[2])
     elif year == 3:
-        print('You are a junior in undergrad.')
+        print(collegeTitles[3])
     elif year == 4:
-        print('You are a senior in undergrad.')
+        print(collegeTitles[4])
     elif year == 5 or year == 6:
         print('You are in a masters program and in grad school.')
     elif year >= 7 and year <= 10:
@@ -21,3 +24,17 @@ def gradeToTitle():
         print('Something went wrong. please check your input.')
     
 gradeToTitle()
+
+
+# A list is a data type for collecting
+# and organizng other data types.
+# You create a list with a variable name and by square brackets.
+
+listB = [10, 102, 4904]
+
+listOfData = ['words and characters', 10, 10.2324, True, False, listB]
+
+# print(listOfData)
+
+collegeTitles= ['You dont have a college title yet', 'freshman', 'sophmore', 'junior', 'senior']
+
