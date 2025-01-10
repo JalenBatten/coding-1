ca3bec51e4b218d7d9ea768f2ac4532d451630ad
JalenBatten <jbatten396@boyslatin.net>
2025-01-10 14:25:25 +0000

    Async Activity January 10th

---
+++ unit 4/asyncActivityJan10.py
@@ -0,0 +1,19 @@
+frozenFoodAisle=['sausage, pizza, wafles, cheesesteak, ice cream']
+
+internationalFoodAisle=['sushi, curry, pasta, churros, tortillas']
+
+produceFoodAisle=['Apples, Bananas, Oranges, Lemons, Potatoes']
+
+numbers = [1,30,39,50,293,100]
+
+print(numbers[0])
+print(numbers[3])
+print(numbers[4])
+
+def indexFourMultiplier(value):
+    numbers = [1,30,39,50,293,100]
+    x = numbers[4]
+    print(x*value)
+
+
+indexFourMultiplier(3)
