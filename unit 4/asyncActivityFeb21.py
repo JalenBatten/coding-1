e519a7c8e352c76839863ce11dc40ddaa45101ec
JalenBatten <jbatten396@boyslatin.net>
2025-02-21 14:28:14 +0000

    Async Activity February 21

---
+++ unit 4/asyncActivityFeb21.py
@@ -0,0 +1,7 @@
+# The name of my game is Turbo Gunner.
+# It will be a shooter game.
+# The game will have multiple levels thatcan be ranked based on style.
+# It takes takes inspiration from Jetpack Joyride, Doom, and Ultrakill.
+# Jetpack Joyride for the flight, Doom for weapons, and Ultrakill for movement and style system.
+# There wil be power ups that allow people to get a damage buff, health, or a jetpack.
+# Players will be able to switch through multiple weapons with different effects.
